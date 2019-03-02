1ef49b5264cb2ced3eac8a46e691b544e3638834
Ekscursionist <larakek@gmail.com>
2019-03-02 13:38:24 -0500

    backgroudn working, container created and glowing

--- igbtoc/src/Components/Container.jsx
+++ igbtoc/src/Components/Container.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { Layout } from "antd";
+import Navbar from "./Navbar";
 
-const { Header, Footer, Content } = Layout;
+const { Content } = Layout;
 
 class Container extends React.Component {
   render() {
     return (
       <div>
         <Layout>
-          <Header>Header</Header>
-          <Content>Content</Content>
-          <Footer>Footer</Footer>
+          <Content>
+            <Navbar />
+            TESTING TESTING 123S
+            <h6>
+              Note: picture credit to 2015 Aug 30 APOD, a 1955 picture taken by
+              Robert Gendler.
+            </h6>
+          </Content>
         </Layout>
       </div>
     );
